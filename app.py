57e5fa6acbe9b80269458160272fb096423b088a
jvescio1996 <125915743+jvescio1996@users.noreply.github.com>
2023-03-27 14:20:20 -0600

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm, ReservationForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
 posts = [
     {
@@ -62,4 +63,4 @@ def login():
 
 
 if __name__ == '__main__':
-    app.run
+    app.run(debug=True)
