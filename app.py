b7a15c80bf130300f78fe3d36030db7663936dd8
jvescio1996 <125915743+jvescio1996@users.noreply.github.com>
2023-04-06 12:47:16 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -5,9 +5,38 @@ from forms import RegistrationForm, LoginForm, ReservationForm
 import pymysql
 import hashlib
 from passlib.hash import sha256_crypt
-
+import time
+import schedule
 
 salt = "3b0"
+
+res_6AM = [False, False, False, False]
+res_8AM = [False, False, False, False]
+res_10AM = [False, False, False, False]
+res_12PM = [False, False, False, False]
+res_2PM = [False, False, False, False]
+res_4PM = [False, False, False, False]
+res_6PM = [False, False, False, False]
+res_8PM = [False, False, False, False]
+res_10PM = [False, False, False, False]
+
+def wipe_res_6AM(res_6AM):
+    res_6AM = [False, False, False, False]
+    return res_6AM
+
+schedule.every().day.at("00:00").do(wipe_res_6AM,res_6AM)
+
+def checkslot (spot,reservation, res_time):
+        i=0
+        while i < len(spot):
+            print("checkslot")
+            if spot[i] == True:
+                i = i + 1
+                if i >= len(spot):
+                    flash(f'Sorry no spaces available for {res_time}', 'error')
+            elif spot[i] == False:
+                spot[i] = reservation
+                return
 
 pymysql.install_as_MySQLdb()
 
@@ -53,14 +82,113 @@ def about():
 @app.route("/reservation", methods=['GET', 'POST'])
 def reservation():
     form = ReservationForm()
+    cnxn = pymysql.connect(user="dpaAdmin", password="XN@mV9ztP4fe", host="dynamicparking.mysql.database.azure.com", port=3306, database="dynamicparking", ssl_ca="DigiCertGlobalRootCA.crt.pem", ssl_disabled=True)
+    cursor = cnxn.cursor()
+    cursor2 = cnxn.cursor()
+
+    last_row = cursor2.execute("SELECT * FROM camera ORDER BY camera_time DESC LIMIT 1")
+    
+    print(last_row)
+    last_row = cursor2.fetchone()
+    print(last_row)
+    
+    total_spaces = 4
+    camera_occupied = last_row[1]
+    camera_empty = last_row[2]
+    percent_available = (camera_empty / total_spaces) * 100
+
+    #print(camera_occupied)
+    #print(camera_empty)
+    
+    license_plate  = form.license_plate.data
+    res_date = form.reservation_date.data
+    reservation_time = form.reservation_block.data
+    handicap = form.handicap_parking.data
+    
+    """
+    def checkSlot (spot,reservation, res_time):
+        i=0
+        while i < len(spot):
+            print("hi")
+            if spot[i] == True:
+                i = i + 1
+                if i >= len(spot):
+                    flash(f'Sorry no spaces available for {res_time}', 'error')
+            elif spot[i] == False:
+                spot[i] = reservation
+                return
+                """
+
+    #try:
     if form.validate_on_submit():
+        print("hey")
+        if 'loggedin' in session:
+            print("logged in")
+            #print(session['username'])
+            user_id = session['id']
+            user_name = session['username']
+            #print(license_plate, res_date, reservation_time , handicap)
+            if reservation_time == '0':
+                flash(f'Please Select a Time!', 'error')
+                return redirect(url_for('reservation'))
+            elif reservation_time == '1':
+                res_time = "6:00AM"
+                print("6AM")
+                checkslot( res_6AM, True, res_time)
+            elif reservation_time == '2':
+                res_time = "8:00AM"
+                print("8AM")
+                checkslot( res_8AM, True, res_time)
+            elif reservation_time == '3':
+                res_time = "10:00AM"
+                print("10AM")
+                checkslot( res_10AM, True, res_time)
+            elif reservation_time == '4':
+                res_time = "12:00PM"
+                print("12PM")
+                checkslot( res_12PM, True, res_time)
+            elif reservation_time == '5':
+                res_time = "2:00PM"
+                print("2PM")
+                checkslot( res_2PM, True, res_time)
+            elif reservation_time == '6':
+                res_time = "4:00PM"
+                print("4PM")
+                checkslot( res_4PM, True, res_time)
+            elif reservation_time == '7':
+                res_time = "6:00PM"
+                print("6PM")
+                checkslot( res_6PM, True, res_time)
+            elif reservation_time == '8':
+                res_time = "8:00PM"
+                print("8PM")
+                checkslot( res_8PM, True, res_time)
+            elif reservation_time == '9':
+                res_time = "10:00PM"
+                print("10PM")
+                checkslot( res_10PM, True, res_time)
+            else:
+                flash(f'Error with Time Selection')
+                print("res else")
+                #return redirect(url_for('reservation'))
 
+            print(license_plate, res_date, res_time, handicap)
 
-        flash(f'Reservation created for {form.username.data}!', 'success')
-        #flash(f'Reservation created for KFCGroup!', 'success')
+            cursor.execute('INSERT INTO reservations(user_id, plate, reservation_date, reservation_time, handicap) VALUES (%s, %s, %s, %s, %s)', (int(user_id), license_plate, res_date, res_time, int(handicap)))
+            cnxn.commit()
+
+            print("here")
+            flash(f'Reservation Successfully Created for {user_name}!', 'success')
+            #return redirect(url_for('home'))
+        else:
+            print(" login else")
+            flash(f'Please Login before Creating a Reservation!', 'error')
+            return redirect(url_for('login'))
         return redirect(url_for('home'))
-    return render_template('reservation.html', title='Reservation', form=form)
+    #except:
+    #return redirect(url_for('home'))
 
+    return render_template('reservation.html', title='Reservation', form=form, value = total_spaces, value2 = camera_empty, value3 = percent_available)
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
@@ -68,7 +196,6 @@ def register():
     cnxn = pymysql.connect(user="dpaAdmin", password="XN@mV9ztP4fe", host="dynamicparking.mysql.database.azure.com", port=3306, database="dynamicparking", ssl_ca="DigiCertGlobalRootCA.crt.pem", ssl_disabled=True)
     cursor = cnxn.cursor()
     
-    #def user_register(username, password, email):
     username = form.username.data
     password = form.password.data
     email = form.email.data
@@ -83,7 +210,7 @@ def register():
 
             #user_password = str(password)+salt
             db_password = sha256_crypt.encrypt(password)
-            db_password2 = sha256_crypt.encrypt(password)
+            #db_password2 = sha256_crypt.encrypt(password)
 
             print(db_password)
             print(db_password2)
@@ -92,7 +219,7 @@ def register():
             #hpass =hashlib.md5(user_password.encode())
             #print(hpass.hexdigest())
 
-            cursor.execute('INSERT INTO users(username, user_password, user_email) VALUES (%s, %s, %s)', (username, password, email,))
+            cursor.execute('INSERT INTO users(username, user_pass, user_email) VALUES (%s, %s, %s)', (username, db_password, email,))
             cnxn.commit()
 
             flash(f'Account created for {form.username.data}!', 'success')
@@ -116,8 +243,9 @@ def login():
     #def user_login(password, email):
     username = form.username.data
     password = form.password.data
-    print(username)
-    print(password)
+
+    print(type(username))
+    print(type(password))
     
     if form.validate_on_submit():
         """
@@ -129,23 +257,21 @@ def login():
         print(sha256_crypt.verify("P@ssw0rd", db_password))
         """
 
-        cursor.execute('SELECT * FROM users WHERE username = %s AND user_password =  %s', (username, password,))
+        cursor.execute('SELECT * FROM users WHERE username = %s', (username,))
         account = cursor.fetchone()
-        print(account)
         
-        """
-        myresult = cursor.fetchall()
-        for x in myresult:
-        	print(x)
-        """
+        #print(sha256_crypt.verify(password, account[2]))
+        
+        
         
         # If account exists in form table in our database
         if account:
             print("Credentials Match!")
             # Create session data, we can access this data in other routes
             session['loggedin'] = True
-            #session['id'] = account['id']
-            #session['username'] = account['username']
+            session['id'] = account[0]
+            session['username'] = account[1]
+            #session.pop('notlogged_in')
             
             # Redirect to home page
             flash(f'{form.username.data} Logged in Successfully!', 'success')
@@ -164,6 +290,7 @@ def logout():
     session.pop('loggedin', None)
     session.pop('id', None)
     session.pop('username', None)
+    session.clear()
     # Redirect to login page
     flash(f'Logged out Successfully!', 'success')
     return redirect(url_for('login'))
--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, RadioField, DateField, TimeField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, RadioField, DateField, TimeField, SelectField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 import pymysql
 
@@ -17,12 +17,13 @@ class LoginForm(FlaskForm):
 	submit = SubmitField('Login')
 
 class ReservationForm(FlaskForm):
-	username = StringField('KFCGroup', validators=[DataRequired()])
-	license_plate = StringField('License Plate', validators=[DataRequired(), Length(min=1, max=8)])
-	reservation_date = DateField('Reservation Date: ',format='%Y-%m-%d')
-	reservation_start_time  = TimeField('Reservation Start Time',format='%H')
-	reservation_end_time  = TimeField('Reservation End Time',format='%H:')
-	handicap_parking = RadioField('Is Handicap Parking Required?', choices =[('value', 'Yes'),('value_two', 'No')], default='value_two')
+	#total_spots = StringField('Total Spots ', )
+	#available spots = StringField('Available Spots')
+	license_plate = StringField('License Plate ', validators=[DataRequired(), Length(min=1, max=8)])
+	reservation_date = DateField('Reservation Date ',format='%Y-%m-%d')
+	reservation_block  = SelectField(u'Reservation Time (2 hour time periods)', choices =[('0','Select a Time Slot'), ('1','6:00AM'),
+	('2','8:00AM'), ('3','10:00AM'), ('4','12:00PM'), ('5','2:00PM'), ('6','4:00PM'), ('7','6:00PM'), ('8','8:00PM'), ('9','10:00PM')])
+	handicap_parking = RadioField('Is Handicap Parking Required?', choices =[(1, 'Yes'),(0, 'No')], default = 0)
 	submit = SubmitField('Reserve')
 
 
